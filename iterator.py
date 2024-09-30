c54d70f9eeea38d30fb9be02aaeeb1aafc01f351
mahdipoori <mahdipoori3@protonmail.com>
2024-09-30 11:46:45 +0330

    magic fun added with main

--- iterator.py
+++ iterator.py
@@ -3,11 +3,11 @@ class BoxIterator:
         self._items = items
         self._index = 0
 
-    def has_next(self):
-        return self._index < len(self._items)
+    def __iter__(self):
+        return self
 
-    def next(self):
-        if self.has_next():
+    def __next__(self):
+        if self._index < len(self._items):
             item = self._items[self._index]
             self._index += 1
             return item
@@ -19,14 +19,11 @@ class Box:
     def __init__(self, items):
         self._items = items
 
-    def get_iterator(self):
+    def __iter__(self):
         return BoxIterator(self._items)
 
-
+if __name__=="__main__":
 # استفاده از iterator برای پیمایش عناصر در جعبه
-box = Box(["Toy", "Book", "Pen", "Notebook"])
-iterator = box.get_iterator()
-
-while iterator.has_next():
-    item = iterator.next()
-    print(item)
+    box = Box(["Toy", "Book", "Pen", "Notebook"])
+    for item in box:
+         print(item)

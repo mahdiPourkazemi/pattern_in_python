a6b817174a3b6912256caea4dc0f916c368c4658
mahdipoori <mahdipoori3@protonmail.com>
2024-09-30 13:04:31 +0330

    init observer

---
+++ observer.py
@@ -0,0 +1,34 @@
+class Observer:
+    def update(self, message):
+        pass
+
+
+class Channel:
+    def __init__(self):
+        self.observers = []
+
+    def add_observer(self, observer):
+        self.observers.append(observer)
+
+    def send_message(self, message):
+        for observer in self.observers:
+            observer.update(message)
+
+
+class User(Observer):
+    def __init__(self, name):
+        self.name = name
+
+    def update(self, message):
+        print(f"{self.name} received: {message}")
+
+
+if __name__ == "__main__":
+    chat_channel = Channel()
+    user1 = User("Ali")
+    user2 = User("Reza")
+
+    chat_channel.add_observer(user1)
+    chat_channel.add_observer(user2)
+
+    chat_channel.send_message("Hello everyone!")

e04cd60b0f87038e7b39892034690f72a2675ca9
mahdipoori <mahdipoori3@protonmail.com>
2024-09-30 16:15:53 +0330

    init strategy

---
+++ strategy.py
@@ -0,0 +1,35 @@
+# Strategy Interface
+class DiscountStrategy:
+    def apply_discount(self, amount):
+        pass
+
+# Concrete Strategies
+class ChristmasDiscount(DiscountStrategy):
+    def apply_discount(self, amount):
+        return amount * 0.8  # 20% تخفیف
+
+class BlackFridayDiscount(DiscountStrategy):
+    def apply_discount(self, amount):
+        return amount * 0.5  # 50% تخفیف
+
+class NoDiscount(DiscountStrategy):
+    def apply_discount(self, amount):
+        return amount  # بدون تخفیف
+
+# Context
+class ShoppingCart:
+    def __init__(self, discount_strategy):
+        self.discount_strategy = discount_strategy
+
+    def calculate_total(self, amount):
+        return self.discount_strategy.apply_discount(amount)
+if __name__=="__main__":
+    # استفاده از استراتژی‌های مختلف
+    cart = ShoppingCart(ChristmasDiscount())
+    print(cart.calculate_total(100))  # خروجی: 80.0
+
+    cart = ShoppingCart(BlackFridayDiscount())
+    print(cart.calculate_total(100))  # خروجی: 50.0
+
+    cart = ShoppingCart(NoDiscount())
+    print(cart.calculate_total(100))  # خروجی: 100.0

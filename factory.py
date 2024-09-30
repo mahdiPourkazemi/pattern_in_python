51426af99f61e1feb55a4c8c4b5f6acee58f4e38
mahdipoori <mahdipoori3@protonmail.com>
2024-09-30 13:02:25 +0330

    less class and delete bilerplate code

--- factory.py
+++ factory.py
@@ -5,45 +5,50 @@ from abc import ABC, abstractmethod
 class ClimateControlSystem(ABC):
 
     @abstractmethod
-
     def control(self):
-
         pass
-
+    @abstractmethod
+    def __str__(self):
+        pass
 # ۲. کلاس‌های مختلف برای هر نوع دستگاه
 class Cooler(ClimateControlSystem):
     def control(self):
         return "Cooling the house"
+    def __str__(self):
+        return "it is cooler"
 
 class Heater(ClimateControlSystem):
     def control(self):
         return "Heating the house"
-
+    def __str__(self):
+        return "it is heater"
 class Dehumidifier(ClimateControlSystem):
     def control(self):
         return "Dehumidifying the air"
-
+    def __str__(self):
+        return "it is dehumidifier"
 # ۳. کلاس‌های کارخانه‌ای برای هر نوع دستگاه
-class CoolerFactory:
-    def create_device(self) -> ClimateControlSystem:
+class Factory:
+    def cooler_device(self) -> ClimateControlSystem:
         return Cooler()
 
-class HeaterFactory:
-    def create_device(self) -> ClimateControlSystem:
+    def heater_device(self) -> ClimateControlSystem:
         return Heater()
 
-class DehumidifierFactory:
-    def create_device(self) -> ClimateControlSystem:
+    def Dehumidifier_device(self) -> ClimateControlSystem:
         return Dehumidifier()
 
 # ۴. تابعی برای انتخاب کارخانه مناسب بر اساس شرایط
 def get_climate_control_factory(temperature, humidity):
     if temperature > 24:
-        return CoolerFactory()
+        return Factory().cooler_device()#,Factory().Dehumidifier_device() #my change
     elif temperature < 24:
-        return HeaterFactory()
+        return Factory().heater_device()
     elif humidity > 50:
-        return DehumidifierFactory()
+        return Factory().Dehumidifier_device()
 
 if __name__ == "__main__":
-    print(get_climate_control_factory(22,30))
+    
+    print(get_climate_control_factory(50,80))
+    print(get_climate_control_factory(50,80).control())
+    #print(get_climate_control_factory(50,80).create_device())
